69c09ee6e6c58bdf34aa62fcbd9b6db2976d30ef
Shaoming Xiao <u6583177@anu.edu.au>
2026-08-16 19:16:57 +1000

    fix syntax

--- rotate_logs.sh
+++ rotate_logs.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 
 archive_dir=$1
-log_dir=$2for f in $(ls $log_dir/*.log); do
-  age=$(find $f -mtime +7)if [ $age ]; thenmv $f $archive_dir/
-    count=$count+1fidoneecho "Archived $count files"
+log_dir=$2 
+for f in $(ls $log_dir/*.log); do
+  age=$(find $f -mtime +7)
+  if [ $age ]; then 
+    mv $f $archive_dir/
+    count=$count+1 
+  fi 
+done 
+echo "Archived $count files"
