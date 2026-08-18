2150dd5ee34f38327e8523702e87986df837705a
Shaoming Xiao <u6583177@anu.edu.au>
2026-08-19 00:45:26 +1000

    correct count

--- rotate_logs.sh
+++ rotate_logs.sh
@@ -6,11 +6,12 @@ if [ "$#" != 2 ] ;then
 fi
 archive_dir=$1
 log_dir=$2 
+count=0
 for f in “$log_dir”/*.log; do
-  age=$(find $f -mtime +7)
-  if [ $age ]; then 
-    mv $f $archive_dir/
-    count=$count+1 
+  age=$(find "$f" -mtime +7)
+  if [ "$age" ]; then 
+    mv "$f" "$archive_dir"/
+    count=$((count+1)) 
   fi 
 done 
 echo "Archived $count files"
